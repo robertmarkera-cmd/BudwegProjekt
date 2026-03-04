4651a59a58e0e4ec830577b2b817f309d081018f
Amin Christiansen <Amin081204@gmail.com>
2026-03-04 13:57:34 +0100

    Views implementeret

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Budweg.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            // open CreateCaliberPage in a new Window
+            var page = new CreateCaliberPage();
+            var win = new Window
+            {
+                Title = "Opret Kaliber",
+                Content = page,
+                Owner = this,
+                Width = 380,
+                Height = 700,
+                WindowStartupLocation = WindowStartupLocation.CenterOwner,
+                ResizeMode = ResizeMode.NoResize
+            };
+
+            win.ShowDialog();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+            => Application.Current.Shutdown();
+    }
+}
