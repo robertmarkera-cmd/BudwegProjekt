be758de41276470bf83a5833fdf0aeba8645b25b
Amin Christiansen <Amin081204@gmail.com>
2026-03-17 14:15:13 +0100

    - billed

--- View-Models/CaliberRepository.cs
+++ View-Models/CaliberRepository.cs
@@ -2,8 +2,6 @@
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System.Data;
-using System.IO;
-using System.Text.Json;
 
 namespace Budweg.View_Models
 {
@@ -22,18 +20,25 @@ namespace Budweg.View_Models
 
         public void AddCaliber(Caliper cal)
         {
-            
-
             using var con = new SqlConnection(ConnectionString);
             using var cmd = new SqlCommand("dbo.CreateCaliber", con);
 
             cmd.CommandType = CommandType.StoredProcedure;
 
-            cmd.Parameters.Add("@Type", SqlDbType.NVarChar, 80).Value = (object?)cal.Type ?? DBNull.Value;
-            cmd.Parameters.Add("@Comment", SqlDbType.NVarChar, 255).Value = (object?)cal.Comment ?? DBNull.Value;
+            cmd.Parameters.Add("@Type", SqlDbType.NVarChar, 80).Value =
+                (object?)cal.Type ?? DBNull.Value;
+
+            cmd.Parameters.Add("@Comment", SqlDbType.NVarChar, 255).Value =
+                (object?)cal.Comment ?? DBNull.Value;
+
+            cmd.Parameters.Add("@Picture", SqlDbType.VarBinary, -1).Value = DBNull.Value;
+
             cmd.Parameters.Add("@ItemNumber", SqlDbType.Int).Value = cal.FrameID;
-            cmd.Parameters.Add("@Brand", SqlDbType.NVarChar, 80).Value = (object?)cal.Manufacturer ?? DBNull.Value;
-            cmd.Parameters.Add("@BatchID", SqlDbType.Int).Value = cal.BatchID; // REQUIRED
+
+            cmd.Parameters.Add("@Brand", SqlDbType.NVarChar, 80).Value =
+                (object?)cal.Manufacturer ?? DBNull.Value;
+
+            cmd.Parameters.Add("@BatchID", SqlDbType.Int).Value = cal.BatchID;
 
             con.Open();
             var result = cmd.ExecuteScalar();
