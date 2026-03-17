0991196e32da9b790e95c0944619ceef3b94198d
Amin Christiansen <Amin081204@gmail.com>
2026-03-17 13:34:32 +0100

    sidste push

--- Commands/SaveCommand.cs
+++ Commands/SaveCommand.cs
@@ -1,20 +1,33 @@
+using Budweg.Models;
+using Budweg.View_Models;
 using System;
 using System.Windows.Input;
 
 namespace Budweg.Commands
 {
-    public class SaveCommand : ICommand
+    public class AddCaliperCommand : ICommand
     {
+        private readonly CaliberRepository _repository;
+
+        public AddCaliperCommand(CaliberRepository repository)
+        {
+            _repository = repository;
+        }
+
         public event EventHandler? CanExecuteChanged;
 
         public bool CanExecute(object? parameter)
         {
-            return true;
+            // Hvis du vil validere input senere, gøres det her.
+            return parameter is Caliper;
         }
 
         public void Execute(object? parameter)
         {
-            throw new NotImplementedException();
+            if (parameter is Caliper caliper)
+            {
+                _repository.AddCaliber(caliper);
+            }
         }
     }
 }
--- View-Models/CaliberRepository.cs
+++ View-Models/CaliberRepository.cs
@@ -1,8 +1,11 @@
-﻿using Budweg.Models;
-using Microsoft.Data.SqlClient;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing; 
 using System.IO;
 using System.Text.Json;
-using System.Data;
+using Budweg.Models;
+using Microsoft.Data.SqlClient;
 
 namespace Budweg.View_Models
 {
@@ -10,52 +13,85 @@ namespace Budweg.View_Models
     {
         private readonly string ConnectionString;
 
-        private List<Caliper> _caliper = new List<Caliper>();
+        private readonly List<Caliper> _caliper = new List<Caliper>();
 
         public CaliberRepository()
         {
+            var baseDir = AppContext.BaseDirectory;
+            var cfgPath = Path.Combine(baseDir, "jsconfig1.json");
 
-            var json = File.ReadAllText("jsconfig1.json");
-            using var doc = JsonDocument.Parse(json);
-            if (doc.RootElement.TryGetProperty("ConnectionStrings", out var cs)
-                && cs.TryGetProperty("MyDBConnection", out var conn))
+            if (!File.Exists(cfgPath))
             {
-                ConnectionString = conn.GetString() ?? string.Empty;
+                cfgPath = "jsconfig1.json";
+            }
+
+            if (File.Exists(cfgPath))
+            {
+                var json = File.ReadAllText(cfgPath);
+                using var doc = JsonDocument.Parse(json);
+
+                if (doc.RootElement.TryGetProperty("ConnectionStrings", out var cs)
+                    && cs.TryGetProperty("MyDBConnection", out var conn))
+                {
+                    ConnectionString = conn.GetString() ?? string.Empty;
+                }
+                else
+                {
+                    ConnectionString = string.Empty;
+                }
             }
             else
             {
                 ConnectionString = string.Empty;
             }
 
-
+            if (string.IsNullOrWhiteSpace(ConnectionString))
+            {
+                throw new InvalidOperationException(
+                    "Connection string 'MyDBConnection' blev ikke fundet i jsconfig1.json.");
+            }
         }
+
         public void AddCaliber(Caliper caliperToBeCreated)
         {
+            if (caliperToBeCreated is null)
+                throw new ArgumentNullException(nameof(caliperToBeCreated));
 
             byte[]? pictureBytes = null;
             if (caliperToBeCreated.Picture != null)
             {
-                using (var ms = new System.IO.MemoryStream())
+                using (var ms = new MemoryStream())
                 {
                     caliperToBeCreated.Picture.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
                     pictureBytes = ms.ToArray();
                 }
             }
 
-            using (SqlConnection con = new SqlConnection(ConnectionString))
+            using (var con = new SqlConnection(ConnectionString))
             {
                 con.Open();
 
-                using (SqlCommand cmd = new SqlCommand("dbo.CreateCaliber", con))
+                using (var cmd = new SqlCommand("dbo.CreateCaliber", con))
                 {
                     cmd.CommandType = CommandType.StoredProcedure;
 
-                    cmd.Parameters.Add("@Type", SqlDbType.NVarChar, 80).Value = (object?)caliperToBeCreated.Type ?? DBNull.Value;
-                    cmd.Parameters.Add("@Comment", SqlDbType.NVarChar, 255).Value = (object?)caliperToBeCreated.Comment ?? DBNull.Value;
-                    cmd.Parameters.Add("@Picture", SqlDbType.VarBinary, -1).Value = (object?)pictureBytes ?? DBNull.Value;
-                    cmd.Parameters.Add("@ItemNumber", SqlDbType.Int).Value = caliperToBeCreated.FrameID;
-                    cmd.Parameters.Add("@Brand", SqlDbType.NVarChar, 80).Value = (object?)caliperToBeCreated.Manufacturer ?? DBNull.Value;
-                    cmd.Parameters.Add("@BatchID", SqlDbType.Int).Value = DBNull.Value;
+                    cmd.Parameters.Add("@Type", SqlDbType.NVarChar, 80)
+                       .Value = (object?)caliperToBeCreated.Type ?? DBNull.Value;
+
+                    cmd.Parameters.Add("@Comment", SqlDbType.NVarChar, 255)
+                       .Value = (object?)caliperToBeCreated.Comment ?? DBNull.Value;
+
+                    var picParam = cmd.Parameters.Add("@Picture", SqlDbType.VarBinary, -1);
+                    picParam.Value = (object?)pictureBytes ?? DBNull.Value;
+
+                    cmd.Parameters.Add("@ItemNumber", SqlDbType.Int)
+                       .Value = caliperToBeCreated.FrameID;
+
+                    cmd.Parameters.Add("@Brand", SqlDbType.NVarChar, 80)
+                       .Value = (object?)caliperToBeCreated.Manufacturer ?? DBNull.Value;
+
+                    cmd.Parameters.Add("@BatchID", SqlDbType.Int)
+                       .Value = DBNull.Value;
 
                     var result = cmd.ExecuteScalar();
                     if (result != null && int.TryParse(result.ToString(), out var newId))
